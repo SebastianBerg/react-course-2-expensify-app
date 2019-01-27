75d00975fb39c54e1b54939161babe525cc5f6e1
Sebastian Berg <s.berg@live.se>
2019-01-27 10:16:52 +0100

    styled the modal

--- src/components/partials/OptionModal.js
+++ src/components/partials/OptionModal.js
@@ -6,6 +6,23 @@ const OptionModal = props => (
     isOpen={!!props.isOpen}
     ariaHideApp={false}
     onRequestClose={props.handleCloseModal}
+    style={{
+      content: {
+        border: "2px solid #eb560a",
+        borderRadius: "4px",
+        bottom: "auto",
+        minHeight: "10rem",
+        left: "50%",
+        padding: "2rem",
+        position: "fixed",
+        right: "auto",
+        top: "50%",
+        transform: "translate(-50%,-50%)",
+        minWidth: "20rem",
+        width: "80%",
+        maxWidth: "60rem"
+      }
+    }}
   >
     <div className="modal-box">
       <h1 className="modal-title">
